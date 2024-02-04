cfbc23964557f30db53cd19654a1fd64d05ba5c7
Anmol <nmol.crestha@gmail.com>
2024-02-04 07:05:31 +0545

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section>
+      <div className='container'>
+        <h1 className='m-2 text-3xl font-bold'>Hello</h1>
+      </div>
+    </section>
+  )
+}
